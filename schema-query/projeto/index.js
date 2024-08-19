8e209ab478a0a9f2370cc6aabe18d8ad08ac77d5
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2024-08-19 13:40:30 -0300

    first query

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,8 +1,17 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    # API entry points
+    type Query {
+        hello: String!
+    }
+`;
 
-const resolvers = {};
+const resolvers = {
+    Query: {
+        hello: () => "Hello, world!",
+    },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
