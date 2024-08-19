9d5477ddcf9d3cfc213bd7fa306877576dae1f9f
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2024-08-19 16:19:53 -0300

    return arrays

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,5 +1,26 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const users = [
+    {
+        id: 1,
+        name: "João Silva",
+        email: "joão@email.com",
+        age: 29,
+    },
+    {
+        id: 2,
+        name: "Rafael Junior",
+        email: "rafael@email.com",
+        age: 31,
+    },
+    {
+        id: 3,
+        name: "Daniela Smith",
+        email: "daniela@email.com",
+        age: 24,
+    },
+];
+
 const typeDefs = gql`
     scalar Hour
 
@@ -25,6 +46,8 @@ const typeDefs = gql`
         currentTime: Hour!
         loggedUser: User
         exampleProduct: Product
+        lotteryNumbers: [Int!]!
+        users: [User]
     }
 `;
 
@@ -62,6 +85,25 @@ const resolvers = {
                 discount: 0.5,
             };
         },
+        lotteryNumbers: () => {
+            const crescent = (a, b) => a - b;
+            const numbers = [];
+
+            while (numbers.length < 6) {
+                const number = parseInt(Math.random() * 60 + 1);
+                if (!numbers.includes(number)) {
+                    numbers.push(number);
+                }
+            }
+            return numbers.sort(crescent);
+            // return Array(6)
+            //     .fill(0)
+            //     .map((n) =>
+            //         checkIfAlreadyExists(parseInt(Math.random() * 60 + 1))
+            //     )
+            //     .sort(crescent);
+        },
+        users: () => users,
     },
 };
 
